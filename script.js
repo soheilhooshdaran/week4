3f0bf67ad750b987ba75e876471b7bb8fe5a947d
soheilhooshdaran <shooshdaran577@gmail.com>
2016-05-26 10:01:13 +0430

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 // should be undefined
 var names =   ["john", "mary", "Hadi", "james",
-           "Hossein", "arthur", "joe", "jim"]
-for (var name in names) 
+           "Hossein", "arthur", "joe", "jim"];
+var helloSpeaker=function(name){
+	console.log("hello "+name);
+}
+var byeSpeaker = function(name){
+	console.log("bye "+name);
+}
+for(var name  in names)
 	if(names[name][0]=='j'){
-		
-	    console.log("Goodbye "+ names[name]);
+		byeSpeaker(names[name]);
 	}
 	else
-		console.log("hello "+names[name]);
+		helloSpeaker(names[name]);
 
-// if (x == undefined) {
-//   console.log("x is undefined");
-// }
-
-// x = 5;
-// if (x == undefined) {
-//   console.log("x is undefined");
-// }
-// else {
-//   console.log("x has been defined");
 // }
 
